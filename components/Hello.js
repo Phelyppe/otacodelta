ef7937dd65c9ee710f42c15aa4574f05d6b02571
Felipe Pinheiro <felipe.pinheiro@live.com>
2022-12-02 19:39:14 -0300

    chore: Test deploy

--- components/Hello.js
+++ components/Hello.js
@@ -2,7 +2,7 @@ export default function Hello() {
   return (
     <>
       <h1>Hello, otacos!</h1>
-      <h2>Em breve teremos algo legal por aqui.</h2>
+      <h2>Em breve teremos algo legal por aqui. :)</h2>
     </>
   )
 }
